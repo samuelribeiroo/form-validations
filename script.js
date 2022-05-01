56e5c1797b241614d44d18f989624c10911cf9be
Samuel Ribeiro <samuelribeirodev@gmail.com>
2022-05-01 17:33:21 -0300

    add: logic validation

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const userName = document.getElementById('username')
 
 const getEmail = document.getElementById('email')
 
-const getPassword = document.getElementById('password2')
+const getPassword = document.getElementById('password')
 
 const passwordAgain = document.getElementById('password2')
 
@@ -38,10 +38,45 @@ function setSuccess(element) {
     inputControl.classList.remove('error')
 }
     
-}
-
 function isValidEmail(email) {
     const regEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
     return regEx.test(String(email).toLocaleLowerCase())
 }
+
+function validateInputs() {
+    const usernameValue = userName.value.trim()
+    const emailValue = getEmail.value.trim()
+    const getPasswordValue = getPassword.value.trim()
+    const passwordAgainValue = passwordAgain.value.trim()
+
+    if(usernameValue === '') {
+      setError(userName, 'Username is required')
+    } else {
+        setSuccess(userName)
+    }
+
+    if(emailValue === '') {
+        setError(getEmail, 'Email is required')
+    } else if (!isValidEmail(emailValue)) {
+        setError(getEmail, 'Please provide a valid email.')
+    } else {
+        setSuccess(getEmail)
+    }
+
+    if(getPasswordValue === '') {
+        setError(getPassword, 'Password is required')
+    } else if (getPasswordValue.length < 8) {
+        setError(getPassword, 'Password must be at least 8 characters')
+    } else {
+        setSuccess(getPassword)
+    }
+
+    if(passwordAgainValue === '')  {
+        setError(passwordAgain, 'Password is required')
+    } else if (passwordAgainValue !== getPasswordValue) {
+        setError(passwordAgain, "Password does't match")
+    } else {
+        setSuccess(passwordAgain)
+    }
+ }
